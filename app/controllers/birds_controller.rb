c953c60846c6551e783889e0da12f439309588f4
Xiaoyi Liu <lxy878@gmail.com>
2020-08-21 01:59:27 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -2,6 +2,6 @@ class BirdsController < ApplicationController
   def index
     @birds = Bird.all
     # render 'birds/index.html.erb'
-    render json: @birds
+    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
   end
 end
