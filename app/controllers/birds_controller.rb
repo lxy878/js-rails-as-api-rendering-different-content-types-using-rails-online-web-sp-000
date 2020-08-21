c68f76625d81ff1570cd1ee0cf47a22f6181c62c
Xiaoyi Liu <lxy878@gmail.com>
2020-08-21 01:56:52 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -2,6 +2,6 @@ class BirdsController < ApplicationController
   def index
     @birds = Bird.all
     # render 'birds/index.html.erb'
-    render json: { message: 'Hashes of data will get converted to JSON' }
+    render json: @birds
   end
 end
